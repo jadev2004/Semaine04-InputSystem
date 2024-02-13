06323a51047adf4ead406b00b2f161d24f5e0bf1
jadev2004 <jade.vaillancourt24@hotmail.com>
2024-02-13 09:55:44 -0500

    code

--- Assets/Scripts/Vaisseau.cs
+++ Assets/Scripts/Vaisseau.cs
@@ -6,6 +6,12 @@ using UnityEngine.InputSystem;
 public class Vaisseau : MonoBehaviour
 {
     // Start is called before the first frame update
+
+private float _valeurX;
+private float _valeurY;
+private Vector2 _mouvementHorizontal;
+
+
     void Start()
     {
         
@@ -14,11 +20,16 @@ public class Vaisseau : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Bouge();
     }
 
     public void RecevoirInfo(InputAction.CallbackContext context){
 
         Debug.Log(context.ReadValue<Vector2>());
+    }
+
+    void Bouge(){
+
+
     }
 }
